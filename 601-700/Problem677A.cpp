af9760d0f5b2c659239a1e9bfa1ddc8d2adbecf1
unknown <mtevfik41@gmail.com>
2021-02-09 22:36:29 +0300

    Problem677A

--- 601-700/Problem677A.cpp
+++ 601-700/Problem677A.cpp
@@ -10,11 +10,9 @@ int main() {
     int n,h;
     cin >> n >> h;
     int count = 0;
-    vector<int> hg;
     for(int i = 0;i < n;i++) {
         int k;
         cin >> k;
-        hg.push_back(k);
         if(k <= h) count++;
         else count+=2;
     }
