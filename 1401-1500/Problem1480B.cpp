2b0145740172036fa351b346e404a817064c8e05
unknown <mtevfik41@gmail.com>
2021-02-13 23:49:02 +0300

    template

--- 1401-1500/Problem1480B.cpp
+++ 1401-1500/Problem1480B.cpp
@@ -14,18 +14,42 @@ _|"""""|_|"""""|_|"""""|_| """"|_| """ |_|"""""|_|"""""|
 #include <cctype>
 #include <algorithm>
 #include <cmath>
-#include <map>
-#include <set>
-#include <list>
 
 #define all(x) x.begin(),x.end()
 #define pb push_back
 
+using namespace std;
+
 typedef long long int ll;
 
-using namespace std;
+void run_case() {
+    ll atc, he, en, sum = 0;
+    cin >> atc >> he >> en;
+    vector<ll> en_atc(en), en_he(en);
+    for(int i = 0 ; i < en ; i++) {
+        cin >> en_atc[i];   
+    }
+    for(int i = 0 ; i < en ; i++) {
+        cin >> en_he[i];
+        ll temp = (en_he[i] + atc - 1) / atc;
+        sum += temp * en_atc[i];   
+    }
+    for(int i = 0 ; i < en ; i++) {
+        if(he > sum - en_atc[i]) {
+            cout << "YES\n";
+            return;
+        }   
+    }
+    cout << "NO\n";
+}
 
 int main() {
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
+    int tests;
+    cin >> tests;
+    while(tests != 0) {
+        run_case();
+        tests--;
+    }
 }
