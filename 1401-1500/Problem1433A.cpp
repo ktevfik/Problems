06e72205df756d54321ed3b853229b94eece82a2
unknown <mtevfik41@gmail.com>
2021-02-16 02:18:14 +0300

    added new solutions

---
+++ 1401-1500/Problem1433A.cpp
@@ -0,0 +1,65 @@
+/*
+ __  __   _____    ___   __   __    ___    ___    _  __  
+|  \/  | |_   _|  | __|  \ \ / /   | __|  |_ _|  | |/ /  
+| |\/| |   | |    | _|    \ V /    | _|    | |   | ' <   
+|_|__|_|  _|_|_   |___|   _\_/_   _|_|_   |___|  |_|\_\  
+_|"""""|_|"""""|_|"""""|_| """"|_| """ |_|"""""|_|"""""| 
+"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-' 
+
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <list>
+
+typedef long long int ll;
+
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vs vector<string>
+#define vc vector<char>
+
+using namespace std;
+
+void run_case() {
+    int a;
+    cin >> a;
+    int b = 1;
+    int as_count = 1;
+    int t_count = 1;
+    while(b != a) {
+        b = b * 10 + as_count;
+        int copy = b;
+        int bsmk = 0;
+        while(copy > 0) {
+            copy/=10;
+            bsmk++;
+        }
+        if(bsmk == 5) {
+            if(b == a) break;
+            as_count++;
+            b = as_count;
+            bsmk = 1;
+        }
+        t_count+=bsmk;
+    }
+    cout << t_count << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int tests;
+    cin >> tests;
+    while(tests != 0) {
+        run_case();
+        tests--;
+    }
+}
